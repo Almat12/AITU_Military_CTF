6f628e394b24fda743b9b8b350e620548b6fd55e
Futur1stXD <126179639+Futur1stXD@users.noreply.github.com>
2024-01-29 13:38:52 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '10.1.14.35',
+    port: 80
+  },
   plugins: [vue()],
 })
