ecedff05438b4cd3ae2f687a081c992c9d357feb
Almat12 <91843179+Almat12@users.noreply.github.com>
2024-02-08 12:32:06 +0600

    Update AppCore.vue

--- src/AppCore.vue
+++ src/AppCore.vue
@@ -73,14 +73,16 @@
 
 
       <!-- Organization -->
-      <header id="key-events" class=" text-black p-4">
-      <h1 class="text-4xl font-bold justify-center items-center flex ">Организаторы</h1>
+      <header id="key-events" class="text-black p-4">
+      <h1 class="text-4xl font-bold justify-center items-center flex">Организаторы</h1>
     </header>
-  <section class="mt-4 p-4 bg-white ">
-        <div class="flex justify-center flex-wrap items-center mt-8 ">
-    <img src="./assets/ctfkaf.png" alt="" class="h-auto w-40 mb-10">
-    <img src="./assets/Astana_IT_University.png" alt="" class="h-auto w-40 ml-20 mr-16">
-
+    <section class="mt-4 p-4 bg-white">
+      <div class="flex flex-wrap justify-center items-center mt-8 md:justify-center">
+        <!-- The first image will be centered on medium devices and up, and left-aligned on smaller screens -->
+        <img src="./assets/ctfkaf.png" alt="" class="h-auto w-44 mb-10 block md:hidden mx-auto">
+        <img src="./assets/logo.png" alt="" class="h-auto w-44 mb-9 rounded-lg">
+        <img src="./assets/ctfkaf.png" alt="" class="h-auto w-44 mb-12 ml-10 mr-10 hidden md:block">
+        <img src="./assets/Astana_IT_University.png" alt="" class="h-auto w-44 mb-10">
       </div>
     </section>
     </div>
